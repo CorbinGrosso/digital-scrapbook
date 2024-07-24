6d5aceeac3a2abb919f9411a7138a48d4f4b7e6d
Corbin Grosso <corbingrosso@gmail.com>
2024-07-24 00:24:51 -0400

    Commented out the pages for creating tables now that the tables are made. These pages threw errors on deployment but only need to be run locally

--- app/api/create-media-table/route.tsx
+++ app/api/create-media-table/route.tsx
@@ -1,18 +1,18 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
+// import { sql } from '@vercel/postgres';
+// import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-    try {
-        const result =
-            await sql`CREATE TABLE Media (
-                id SERIAL PRIMARY KEY,
-                description VARCHAR(255),
-                media BYTEA NOT NULL,
-                post_id INT NOT NULL,
-                FOREIGN KEY (post_id) REFERENCES Posts(id)
-            );`;
-        return NextResponse.json({ result }, { status: 200 });
-    } catch (error) {
-        return NextResponse.json({ error }, { status: 500 });
-    }
-}
+// export async function GET(request: Request) {
+//     try {
+//         const result =
+//             await sql`CREATE TABLE Media (
+//                 id SERIAL PRIMARY KEY,
+//                 description VARCHAR(255),
+//                 media BYTEA NOT NULL,
+//                 post_id INT NOT NULL,
+//                 FOREIGN KEY (post_id) REFERENCES Posts(id)
+//             );`;
+//         return NextResponse.json({ result }, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json({ error }, { status: 500 });
+//     }
+// }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { sql } from "@vercel/postgres";
-
 export default function Home() {
   return (
     <div>Hello World! </div>
