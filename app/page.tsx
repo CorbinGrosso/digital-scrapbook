32b2b1a8436b3f5dd37b5034c6feb87c12cf70d9
Corbin Grosso <corbingrosso@gmail.com>
2024-08-03 19:56:37 -0400

    Altered background color to look better on smaller screens

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,35 @@ import Link from 'next/link';
 import React from 'react';
 
 export default function Home() {
-  const HEADER_TEXT = {
-    'HOME': "J+C Digital Scrapbook",
-    'CREATE_NEW_POST': "New Post"
-  };
 
-  const title = "J+C Digital Scrapbook";
+  function Header() {
+    const TITLE = {
+      'HOME': "J+C Digital Scrapbook",
+      'CREATE_NEW_POST': "New Post"
+    }
+    return <div id="header" >
+      <h1>{TITLE.HOME}</h1>
+    </div>
+  }
+  
+  function Body() {
+  
+  }
+  
+  function Footer() {
+    return <div id="footer" >
+      This site was created with love for Jamie McKnight by Corbin Grosso.
+        < br />
+        Check out his other work, or the source code for this project,
+          at his < a href = "https://www.github.com/CorbinGrosso" >Github</a>.
+            </div>
+  }
+
   return (
     <div>
-    <h1>{ title } </h1>
+    <Header />
 
-    < br />
     <Footer />
     </div>
   );
-}
-
-function header() {
-
-}
-
-function body() {
-
-}
-
-function Footer() {
-  return <div id="footer" >
-    This site was created with love for Jamie McKnight by Corbin Grosso.
-      < br />
-      Check out his other work, or the source code for this project,
-        at his < a href = "https://www.github.com/CorbinGrosso" > Github </a>
-          </div>
 }
