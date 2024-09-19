755841d46dcfe6cc000f1d75c8cad60b487b8839
Corbin Grosso <corbingrosso@gmail.com>
2024-09-19 10:13:20 -0400

    Removed unused import

--- components/AddPostForm.tsx
+++ components/AddPostForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { UploadPostToDatabase } from '@/app/SqlLib'
 
 export default function AddPostForm() {
